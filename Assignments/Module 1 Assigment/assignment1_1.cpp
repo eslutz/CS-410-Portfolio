76ad578eac7e00dbbd7da34132697fb7a3982526
Eric Slutz <eric.slutz@icloud.com>
2023-08-29 17:07:46 -0400

    completed assignment 1

---
+++ Assignments/Module 1 Assigment/assignment1_1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int width = 10;
+    int height = 5;
+    int area;
+    area = width * height;
+
+    cout << endl << area;
+    return 0;
+}
